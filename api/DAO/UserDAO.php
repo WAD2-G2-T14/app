194a39f541ca0f0cf557c6215f38b6f1e4283ac3
Ambrose Ang <ambrose.tk.ang@gmail.com>
2020-11-09 00:16:22 +0800

    added signup functionality

--- api/DAO/UserDAO.php
+++ api/DAO/UserDAO.php
@@ -59,5 +59,55 @@ class UserDAO {
             ];
         }
     }
+
+    public function validateEmail($email) {
+        $database = new ConnectionManager();
+        $pdo = $database->connect();
+
+        $sql = '
+            SELECT *
+            FROM users
+            WHERE email = :email
+        ';
+
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindParam(':email', $email, PDO::PARAM_STR);
+
+        if ($stmt->execute()) {
+            $stmt->setFetchMode(PDO::FETCH_ASSOC);
+            $row = $stmt->fetch();
+
+            if ($row) return FALSE;
+            else return TRUE;
+        }
+    }
+
+    public function signup($userDetails) {
+        $database = new ConnectionManager();
+        $pdo = $database->connect();
+
+        $sql_1 = "INSERT INTO users VALUES (:user_id, :email, :password, :phone_number, :fullname);";
+
+        $stmt_1 = $pdo->prepare($sql_1);
+        $stmt_1->bindParam(':user_id', $userDetails['user_id'], PDO::PARAM_STR);
+        $stmt_1->bindParam(':email', $userDetails['email'], PDO::PARAM_STR);
+        $stmt_1->bindParam(':password', $userDetails['password'], PDO::PARAM_STR);
+        $stmt_1->bindParam(':phone_number', $userDetails['phone_number'], PDO::PARAM_STR);
+        $stmt_1->bindParam(':fullname', $userDetails['fullname'], PDO::PARAM_STR);
+
+        $sql_2 = "INSERT INTO user_addresses VALUES (:user_id, :postal_code, :city, :user_address, :country);";
+        $stmt_2 = $pdo->prepare($sql_2);
+        $stmt_2->bindParam(':user_id', $userDetails['user_id'], PDO::PARAM_STR);
+        $stmt_2->bindParam(':postal_code', $userDetails['postal_code'], PDO::PARAM_STR);
+        $stmt_2->bindParam(':city', $userDetails['city'], PDO::PARAM_STR);
+        $stmt_2->bindParam(':user_address', $userDetails['user_address'], PDO::PARAM_STR);
+        $stmt_2->bindParam(':country', $userDetails['country'], PDO::PARAM_STR);
+
+        if ($stmt_1->execute() && $stmt_2->execute()) {
+            return TRUE;
+        } else {
+            return FALSE;
+        }
+    }
 }
 ?>
