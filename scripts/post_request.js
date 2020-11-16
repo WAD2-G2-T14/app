c43716dc760d7cea560a2b98f2249feb610fa1cd
Ambrose Ang <ambrose.tk.ang@gmail.com>
2020-11-17 00:30:18 +0800

    fix login twice bug

--- scripts/post_request.js
+++ scripts/post_request.js
@@ -1,55 +1,51 @@
 //submitting a post
-    function submitPost(){
-        // document.getElementById('submitBtn').addEventListener('click',submitPost);
+    function submitPost(event){
+        event.preventDefault()
+
         let user = document.getElementById('user').value; //username
         let password = document.getElementById('password').value; //password
-        // e.preventDefault();
 
         fetch("../api/auth/login.php", {
             method:'POST',
             headers: {
-            'Accept': 'application/json',
-            'Content-type':'application/json'
+                'Accept': 'application/json',
+                'Content-type':'application/json'
             },
             body:JSON.stringify({
-            "email" : user,
-            "password" : password
-            }) //equivalent to ({user:user, password:password})
+                "email" : user,
+                "password" : password
+            })
         })
-            .then(response => response.json())
-            .then(json => {
-            if (json.error){
-                // console.log(json.error);
-                alert(json.error);
-            }
-            else if (json["error"]){
+        .then(response => response.json())
+        .then(json => {
+            if (json["error"]) {
                 alert(json["error"] );
             }
-            else{
-            var fullname = json["name"];
-            var phone = json["phone"];
-            var user_id = json["user_id"];
-            var email = json["email"];
-            console.log(json);
-        
-            document.cookie =  JSON.stringify({"user_details": {
-                "id": user_id,
-                "fullname": fullname,
-                "email": email,
-                "phone": phone
-                },
-                "cart": [{'product_id': '20622707'
-                       } ],
-                "product_id": "20622707",
+            else {
+                var fullname = json["name"];
+                var phone = json["phone"];
+                var user_id = json["user_id"];
+                var email = json["email"];
+
+                document.cookie =  JSON.stringify({
+                    "user_details": {
+                        "id": user_id,
+                        "fullname": fullname,
+                        "email": email,
+                        "phone": phone
+                    },
+                    "cart": [],
+                    "product_id": ""
                 })
-            console.log(document.cookie);
-            window.location.href ='../index.html';  
+
+                window.location.href ='../';
             }
         })
-        }
-        
+    }
 
-    function usersignup(){
+    function usersignup(event){
+        event.preventDefault()
+
         let email_address = document.getElementById('email').value;
         let password = document.getElementById('password1').value;
         let name = document.getElementById('fname').value;
@@ -58,33 +54,26 @@
         let city = document.getElementById('city').value;
         let country = document.getElementById('country').value;
         let phone_number = document.getElementById('pnumber').value;
-        console.log(user);
-        console.log(password);
-        // e.preventDefault();
-        
+
         fetch("../api/auth/signup.php", {
             method:'POST',
             headers: {
-            'Accept': 'application/json',
-            'Content-type':'application/json'
+                'Accept': 'application/json',
+                'Content-type':'application/json'
             },
             body:JSON.stringify({
-            "email" : email_address,
-            "password" : password,
-            "name" : name,
-            "postal_code" : postal_code,
-            "address" : address,
-            "city" : city,
-            "country" : country,
-            "phone_number" : phone_number
-
-            }) //equivalent to ({user:user, password:password})
+                "email" : email_address,
+                "password" : password,
+                "name" : name,
+                "postal_code" : postal_code,
+                "address" : address,
+                "city" : city,
+                "country" : country,
+                "phone_number" : phone_number
+            })
         })
-            .then(response => response.json())
-            .then(json => {
-            console.log(json);
+        .then(response => response.json())
+        .then(json => {
             alert('You have signed up! Log in now!');
         });
-        }
-
-    
+    }
